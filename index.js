e4e051600f5aaf46f36668fcb51897cef1fe1153
trungnguyen1997 <61753074+trungnguyen1997@users.noreply.github.com>
2020-08-19 22:59:00 +0700

    update view va update quang cao

--- index.js
+++ index.js
@@ -13,41 +13,12 @@ let hbs = expressHbs.create({
     layoutsDir: __dirname + '/views/Layouts',
     partialsDir: __dirname + '/views/Partials'
 });
+app.use('', require('./Router/Home.admin'));
+
+
+
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.get('/', (req, res) => {
-    res.redirect('/function-1');
-});
-app.get('/function-1', (req, res) => {
-    res.render('./Admin/profile', {
-        page: 'Profile',
-        profile: 'active'
-    })
-});
-app.get('/function-2', (req, res) => {
-    res.render('./Admin/profile', {
-        page: 'Profile',
-        profile: 'active'
-    })
-});
-app.get('/function-3', (req, res) => {
-    res.render('./Admin/profile', {
-        page: 'Profile',
-        profile: 'active'
-    })
-});
-app.get('/function-4', (req, res) => {
-    res.render('./Admin/profile', {
-        page: 'Profile',
-        profile: 'active'
-    })
-});
-app.get('/function-5', (req, res) => {
-    res.render('./Admin/profile', {
-        page: 'Profile',
-        profile: 'active'
-    })
-});
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running at port ${process.env.PORT || 3000}`);
 });
