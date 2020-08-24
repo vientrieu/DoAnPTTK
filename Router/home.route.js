87b691c83222bcee5bd909382dbff1980ba4f96d
vien trieu <37980891+vientrieu@users.noreply.github.com>
2020-08-24 19:56:39 +0700

    update func5

---
+++ Router/home.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const moment = require("moment");
+const app = express.Router();
+app.get('/', (req, res) => {
+    res.redirect('/admin/quanlisanpham');
+});
+app.use('/admin/quanlisanpham/', require('./product.route'));
+app.get('/admin/quanlithongtindathang/dathang/', (req, res) => {
+    res.send("waiting");
+});
+app.use('/admin/quanlithongtindathang/thanhtoan', require('./payment.route'));
+app.use('/admin/advertising/', require('./advertising.route'));
+module.exports = app;
